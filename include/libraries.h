19dc3684c7dd05a5333313c82b5e9a5e9e9e8a34
Leonardo Pasquato <leonardo.pasquato@gmail.com>
2024-02-02 18:23:59 +0100

    libraries: add fundamentals

---
+++ include/libraries.h
@@ -0,0 +1,26 @@
+#ifndef __LIBRARIES_H__
+#define __LIBRARIES_H__
+
+#include <gazebo_msgs/ModelStates.h>
+#include <gazebo_msgs/SetModelState.h>
+#include <gazebo_msgs/SpawnModel.h>
+#include <gazebo_msgs/DeleteModel.h>
+#include <sensor_msgs/HointState.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Pose.h>
+#include <Eigen/Dense>
+#include <cmath>
+#include <vector>
+
+#include <UR5-project/object_array.h>
+#include <UR5-project/output_vision.h>
+#include <UR5-project/spawn_object.h>
+
+#include "ros/ros.h"
+
+
+#endif
+
+
