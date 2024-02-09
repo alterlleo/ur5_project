d10388ec44adf8de09c1c7fc29841066fe0051f2
AlessiaRinaldi <alessia.rinaldi@studenti.unitn.it>
2024-02-09 06:57:39 +0100

    doxygen for stay_away_from and obstacle

--- src/motion-control/Stay_away_from/Obstacle.h
+++ src/motion-control/Stay_away_from/Obstacle.h
@@ -5,11 +5,34 @@
 
 namespace Project {
 
+    /**
+     * @class Obstacle base class
+    */
     class Obstacle {
     public:
+
+        /**
+         * @brief Obstacle class default constructor
+        */
         Obstacle();
+
+        /**
+         * @brief Obstacle specific contructor
+         * @param position: 2D position of the obstacle
+         * @param diameter: ipothetic radial diameter of the obstacle
+         * @param height: ipothetic height of the obstacle
+        */
         Obstacle(Eigen::Vector2d position, double diameter, double height);
+
+        /**
+         * @brief Compute the gradient of a position
+         * @param position: position
+        */
         Eigen::Vector2d gradient(Eigen::Vector2d position);
+
+        /***
+         * @brief Return the obstacle position
+        */
         Eigen::Vector2d getPosition();
 
     protected:
--- src/motion-control/Stay_away_from/Stay_away_from.h
+++ src/motion-control/Stay_away_from/Stay_away_from.h
@@ -10,15 +10,59 @@
 
 namespace Project{
 
+    /**
+     * @class Stay_away_from is used in order to keep some distance between end end effector and obstacles
+    */
     class Stay_away_from{
         public:
+
+            /**
+             * @brief Stay_away_from class default constructor
+            */
             Stay_away_from();
+
+            /**
+             * @brief Stay_away_from specific constructor
+             * @param dims: table dimensions (default obstacle)
+            */
             Stay_away_from(Eigen::Vector2d dims);
 
+            /**
+             * @brief set the start obstacle as a _____sinonimo hill____
+             * @param pos: 2D position of the obstacle
+             * @param d: ipothetical diemeter of the obstacle
+             * @param height: ipothetical height of the obstalce, by default it is set to 1.0
+            */
             void set_start(Eigen::Vector2d pos, double d, double height = 1.0);
+
+            /**
+             * @brief set the target obstacle as a _____sinonimo hill____
+             * @param pos: 2D position of the obstacle
+             * @param d: ipothetical diemeter of the obstacle
+             * @param height: ipothetical height of the obstalce, by default it is set to -1.0
+            */
             void set_target(Eigen::Vector2d pos, double d, double height = -1.0);
+
+            /**
+             * @brief add another obstacle to the list
+             * @param pos: 2D position of the obstacle
+             * @param d: ipothetical diemeter of the obstacle
+             * @param height: ipothetical height of the obstalce, by default it is set to 1.0
+            */
             void add_obstacle(Eigen::Vector2d pos, double d, double height = 1.0);
+
+            /**
+             * @brief Compute the gradient of the position with respect to all the obstacles
+             * @param pos: position
+            */
             Eigen::Vector2d gradient(Eigen::Vector2d pos);
+
+            /**
+             * @brief Compute a safe path between obstacles
+             * @param step: time step between a position and another
+             * @param precision: how much distant we want to be from the table limits
+             * @param max: maximum time
+            */
             std::vector<Eigen::Vector2d> path(double step, double precision, int max);
 
         private:
