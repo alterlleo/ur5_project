0f3b29bbdf7643e505eabed07f016f68762d60f0
AlessiaRinaldi <alessia.rinaldi@studenti.unitn.it>
2024-02-09 07:04:12 +0100

    done utilities, check

--- src/motion-control/checkpoint/checkpoint.h
+++ src/motion-control/checkpoint/checkpoint.h
@@ -6,6 +6,9 @@
 
 namespace Project{
 
+    /**
+     * @class Checkpoint class which stores 3D position and 3D velocity. It is used for intermediate points in a trajectory
+    */
     class Checkpoint{
         public:
             Eigen::Vector3d pos;
--- src/motion-control/utilities/utilities.h
+++ src/motion-control/utilities/utilities.h
@@ -8,10 +8,30 @@
 #include <cmath>
 
 namespace Project{
+
+    /**
+     * @brief Compute the roll, pitch and yaw rotation to a rotation matrix
+     * @param roll
+     * @param pitch
+     * @param yaw
+    */
     Eigen::Matrix3d rpy2rotm(double roll, double pitch, double yaw);
+
+    /**
+     * @brief compute the roll, pitch and yaw rotation to a rotation matrix
+     * @param rpy: 3D vector where roll, pitch and yaw are stored
+    */
     Eigen::Matrix3d rpy2rotm(Eigen::Vector3d rpy);
+
+    /**
+     * @brief Compute the rotation matrix to roll, pitch and yaw 3D vector
+     * @param rotm: rotation matrix
+    */
     Eigen::Vector3d rotm2rpy(Eigen::Matrix3d rotm);
 
+    /**
+     * @brief compute the Roto-Transformation matrix from a 6D vector where are stored 3D position and 3D velocity
+    */
 	Eigen::Matrix4d homTfromRPYXYZ(Eigen::VectorXd p);
 }
 
