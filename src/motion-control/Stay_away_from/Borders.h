e2f7edfb73797de722a34b9bb0f96054f87dc2f4
AlessiaRinaldi <alessia.rinaldi@studenti.unitn.it>
2024-02-10 10:04:05 +0100

    doxygen: Stay_away_from directory

--- src/motion-control/Stay_away_from/Borders.h
+++ src/motion-control/Stay_away_from/Borders.h
@@ -4,12 +4,37 @@
 #include "Obstacle.h"
 
 namespace Project {
-
+    /**
+     * @class Borders class
+    */
     class Borders : public Obstacle {
     public:
+        /**
+         * @brief computes the gradient of a given position with respect to the borders of a geometric shape, returning a vector indicating the direction and intensity of the gradient at that point
+         * @param points: 2D position of the obstacle
+        */
         Eigen::Vector2d gradient(Eigen::Vector2d point);
+        
+        /**
+         * @brief Borders class default constructor
+        */
         Borders();
+
+        /**
+         * @brief Borders specific contructor
+         * @param position: 2D position of the obstacle
+         * @param diameter: ipothetic radial diameter of the obstacle
+         * @param height: ipothetic height of the obstacle
+        */
         Borders(Eigen::Vector2d position, double diameter, double height);
+
+        /**
+         * @brief Borders specific contructor
+         * @param position: 2D position of the obstacle
+         * @param diameter: ipothetic radial diameter of the obstacle
+         * @param height: ipothetic height of the obstacle
+         * @param maxDist: distance from borders
+        */
         Borders(Eigen::Vector2d position, double diameter, double height, double maxDist);
 
     protected:
--- src/motion-control/Stay_away_from/Singularities.h
+++ src/motion-control/Stay_away_from/Singularities.h
@@ -7,8 +7,23 @@ namespace Project {
 
     class Singularities : public Obstacle {
     public:
+        /**
+         * @brief Singularities class default constructor
+        */
         Singularities();
+
+        /**
+         * @brief Singularities specific contructor
+         * @param position: 2D position of the obstacle
+         * @param diameter: ipothetic radial diameter of the obstacle
+         * @param height: ipothetic height of the obstacle
+        */
         Singularities(Eigen::Vector2d position, double diameter, double height);
+
+        /**
+         * @brief compute the gradient of a given position with respect to the singularities, returning a vector indicating the direction and intensity of the gradient at that point
+         * @param point: 2D position of the obstacle
+        */
         Eigen::Vector2d gradient(Eigen::Vector2d point);
 
     protected:
